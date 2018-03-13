3a8e6d5315a4350ff0f645a13040b70cd699b519
nandeeshcm7 <36222483+nandeeshcm7@users.noreply.github.com>
2018-03-14 00:37:41 +0530

    Update Config.php

--- demo/Config.php
+++ demo/Config.php
@@ -2,7 +2,7 @@
 
 //constant database config variable
 
-define("HOST", "den1.mysql5.gear.host");
+define("HOST", "mysql5.gear.host");
 define("USERNAME", "demo9");
 define("PASS", "password@");
 define("DBNAME", "demo9");
