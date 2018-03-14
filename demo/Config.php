92f97103b18a58eb96458cb0d00d8260926d19a4
nandeeshcm7 <36222483+nandeeshcm7@users.noreply.github.com>
2018-03-14 14:09:15 +0530

    q

--- demo/Config.php
+++ demo/Config.php
@@ -2,9 +2,9 @@
 
 //constant database config variable
 
-define("HOST", "den1.mysql5.gear.host");
-define("USERNAME", "demo9");
-define("PASS", "password@");
-define("DBNAME", "demo9");
+define("HOST","den1.mysql5.gear.host");
+define("USERNAME","demo9");
+define("PASS","password@");
+define("DBNAME","demo9");
 
 ?>
